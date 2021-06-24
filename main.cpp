128a19d834728816ae5b255794e509a26a6c1e07
Aleks <aleks@gmail.com>
2021-06-24 19:51:07 +0500

    3.2.7

--- main.cpp
+++ main.cpp
@@ -135,7 +135,14 @@ Input read_input(istream& in, bool prompt)
         {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+
             res = curl_easy_perform(curl);
+            if (res != 0)
+        {
+
+            cerr<< "curl_easy_perform() failed: %s\n"<< curl_easy_strerror(res);
+            exit(1);
+        }
             curl_easy_cleanup(curl);
         }
 
